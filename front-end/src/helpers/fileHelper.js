ffda178b8b7e861748f6404738d436021431e99f
enasskmal <enasskmal@gmail.com>
2019-07-11 10:06:50 +0300

    adding the function excel to json

--- front-end/src/helpers/fileHelper.js
+++ front-end/src/helpers/fileHelper.js
@@ -1,1 +1,19 @@
 // should have all the function excelToJson() and ..
+require('dotenv').config()
+
+// Convert excel file to JSON
+const excelToJson = require('xlsx-to-json');
+ excelToJson({
+    input: "excel.xlsx",
+    output: "output.json",
+    lowerCaseHeaders: true
+ }, function(err, result){
+   if(err){
+     console.error(err);
+   } else {
+     console.log("this is result");
+   }
+ });
+
+const filePath = process.env.file_Path;
+console.log("this is my file path: ", filePath);
