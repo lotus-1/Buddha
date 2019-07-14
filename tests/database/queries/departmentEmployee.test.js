b72fa1d235425ecb593ce38b991cbaefaba432de
aman halabyar <amanhalabyar7@gmail.com>
2019-07-14 14:27:50 +0300

    adding a test to department_employee table

--- tests/database/queries/departmentEmployee.test.js
+++ tests/database/queries/departmentEmployee.test.js
@@ -1,2 +1,14 @@
 const tape = require("tape");
 const runDBbuild = require("../database/db_build");
+
+
+test('testing addDepartmentEmployee function', t => {
+  runDBbuild((err, res) => {
+    t.error(err, 'No Error');
+        addDepartmentEmployee('department_id', 'employee_id', err, result) => {
+        if (err) console.log(err);
+        t.deepEqual(result, true, 'Retun true when adding to the table');
+        t.end();
+      };
+  });
+});
