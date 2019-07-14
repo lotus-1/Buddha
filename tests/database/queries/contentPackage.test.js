4085319622e2403b3b135a022063bb7a5e173154
Jasmineb2011 <Teddybear13.TB@gmail.com>
2019-07-14 14:25:36 +0300

    Added remove test

--- tests/database/queries/contentPackage.test.js
+++ tests/database/queries/contentPackage.test.js
@@ -2,7 +2,7 @@ const tape = require("tape");
 const buildDB = require("../../../buddha-system/src/database/db_build.sql");
 const addContentPackage = require("../../../buddha-system/src/database/queries/addContentPackageQueries");
 const updateContentPackage = require("../../../buddha-system/src/database/queries/addContentPackageQueries");
-
+const removeContentPacksge = require("");
 tape("tape is working", t => {
   t.equals(1, 1, "one equals one");
   t.end();
@@ -11,13 +11,13 @@ tape("tape is working", t => {
 tape("add content package", t => {
   buildDB((err, res) => {
     t.error("this is an error: ", err);
-    let expected = {
+    const expected = {
       package_id: 14682354,
       program_id: 45679235,
       package_level_1: "package level 1",
       package_level_2: "package_level_2",
       package_level_3: "package_level_3",
-      priorty: 5,
+      priority: 5,
       related_wt: "related_wt",
       package_status: "inactive",
       generic: "yes",
@@ -32,16 +32,32 @@ tape("add content package", t => {
   });
 });
 
-tape("update content package by updating priorty", t => {
+tape("update content package by updating priority", t => {
   buildDB((err, res) => {
     t.error("this is an error: ", err);
-    let expected = {
-      priorty: 5
+    const expected = {
+      priority: 5
     };
   });
   updateContentPackage((error, result) => {
     if (err) console.log("this is an error: ", error);
-    t.deepEqual(result, expected, "priorty has been added");
+    t.deepEqual(result, expected, "priority has been added");
     t.end();
+  });
+});
+
+tape("remove a certain content package", t => {
+  buildDB((err, res) => {
+    t.error("this is an error: ", err);
+    const expected = 0;
+    removeContentPacksge((err, res) => {
+      if (err) return err;
+      t.deepEqual(
+        res.rows.length,
+        expected,
+        "removed a certain content package successfully"
+      );
+      t.end();
+    });
   });
 });
