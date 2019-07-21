3d9a5677a78c5529e27556c374ff43fd422f48ae
ahlamkadour1992 <ahlamkadour2016@gmail.com>
2019-07-21 18:54:15 +0300

    data base tables

--- src/back-end/database/db_build.sql
+++ src/back-end/database/db_build.sql
@@ -6,49 +6,42 @@ DROP TABLE IF EXISTS department CASCADE;
 DROP TABLE IF EXISTS work_team CASCADE;
 DROP TABLE IF EXISTS employee CASCADE;
 
--- This table need to be finished in the next sprint
--- CREATE TABLE program (
---   program_id SERIAL PRIMARY KEY,
---   projects
---   program_manager
---   project_lab_related
---   active
--- )
-
+CREATE TABLE program (
+  program_ID SERIAL PRIMARY KEY,
+  projects TEXT NOT NULL,
+  program_Manager TEXT NOT NULL,
+  program_Lab_related TEXT NOT NULL,
+  active BOOLEAN NOT NULL
+);
 
 CREATE TABLE content_package (
   project_ID SERIAL PRIMARY KEY,
-  program_ID INTEGER REFERENCES,
+  program_ID INTEGER,
   related_WT TEXT NOT NULL,
   package_level_1 TEXT NOT NULL,
   package_level_2 TEXT NOT NULL,
-  priority INTEGER LIMIT (10),
+  priority INTEGER CHECK(priority BETWEEN 1 AND 10),
   package_status TEXT NOT NULL,
   -- generic ,
   year INTEGER NOT NULL,
-   [Q419_Flag] INT ,
+  Q419_Flag INT,
   package_level_3 TEXT NOT NULL,
   q TEXT NOT NULL,
   motivation TEXT NOT NULL,
-  related_Sections TEXT NOT NULL,
-);
-
-CREATE TABLE departmentProject (
-  project_ID INTEGER REFERENCES project(id),
-  package_ID INTEGER REFERENCES package(id)
+  related_Sections TEXT NOT NULL
 );
 
 CREATE TABLE department (
   department_id SERIAL PRIMARY KEY,
-  subject VARCHAR(100) NOT NULL
-  -- active
+  subject VARCHAR(100) NOT NULL,
+  active BOOLEAN NOT NULL
 );
 
 CREATE TABLE employee (
+  department_id SERIAL PRIMARY KEY,
   lab TEXT NOT NULL,
   section TEXT NOT NULL,
   department TEXT NOT NULL,
-  department_id SERIAL PRIMARY KEY,
   cost_center INTEGER NOT NULL,
   employee_id INTEGER UNIQUE NOT NULL,
   employee_name TEXT NOT NULL,
@@ -58,10 +51,28 @@ CREATE TABLE employee (
   sw_role TEXT NOT NULL
 );
 
+CREATE TABLE work_team (
+  WT_ID SERIAL PRIMARY KEY,
+  project_ID INTEGER REFERENCES content_package(project_ID),
+  WT_Name TEXT NOT NULL,
+  WT_Leader TEXT NOT NULL,
+  sponsor TEXT NOT NULL,
+  active BOOLEAN NOT NULL,
+  WT_Program_Focus TEXT NOT NULL,
+  WT_group TEXT NOT NULL,
+  sponsors_Lab TEXT NOT NULL,
+  sponsors_Section TEXT NOT NULL,
+  sponsors_Department TEXT NOT NULL
+);
+
 CREATE TABLE department_employee (
-  department_id INTEGER REFERENCES department(department_id)
+  department_id INTEGER REFERENCES department(department_id),
   employee_id INTEGER REFERENCES employee(employee_id)
-  -- % job
+);
+
+CREATE TABLE departmentProject (
+  project_ID INTEGER REFERENCES content_package(project_ID),
+  department_id INTEG ER REFERENCES department(department_id)
 );
 
 
